58901e54fc2d6e65da322fd08d975bb46a43c131
Felipe Milani <felmilani@gmail.com>
2019-03-24 22:31:24 -0300

    Set max width on layout

--- imports/ui/Layout.js
+++ imports/ui/Layout.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Bar from './Bar';
-import withAuth from '../hocs/withAuth';
+import React from 'react'
+import Bar from './Bar'
+import withAuth from '../hocs/withAuth'
 
-const Layout = props =>
-  <div>
+const Layout = props => (
+  <div style={{ backgroundColor: '#f5f5f5', maxWidth: 600, margin: '0 auto' }}>
     <Bar />
-    <div>
-      {props.children}
-    </div>
+    <div>{props.children}</div>
   </div>
+)
 
 export default Layout
 // export default withAuth(Layout)
